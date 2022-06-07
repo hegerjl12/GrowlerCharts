4696835745871b8b4ba99aa7136fc273edb747e6
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-06-07 10:53:00 -0700

    dr y values

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,9 @@ import numpy as np
 def main():
   
   dr_temp_x = np.arange(-60,141)
+  dr_dr_y = np.arange(0.5,1.31, 0.01)
   
-  st.write(dr_temp_x)
+  st.write(dr_dr_y)
   
   
 if __name__ == "__main__":
