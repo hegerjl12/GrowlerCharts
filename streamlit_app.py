0440677385de0dd121f806c890ac3cf4a3cfc08a
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-06-07 12:28:34 -0700

    labels

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,26 +7,23 @@ import altair as alt
 
 def main():
   
-  dr_temp_x = np.arange(-60,150, 10)
-  dr_dr_y = np.arange(0.5,1.31, 0.01, dtype=float)
+  dr_temp_x_input_tendegrees = np.arange(-60,150, 10)
+  dr_temp_x_input_onedegrees = np.arange(-60,141)
+  dr_dr_y_input = np.arange(0.5,1.31, 0.01, dtype=float)
+  
   dr_sealevel = [1.3,1.27,1.23,1.2,1.175,1.15,1.125,1.1,1.08,1.06,1.04,1.02,1.0,0.98,0.96,0.94,0.925,0.91,0.89,0.88,0.86]
   dr_sealevel_array = np.array(dr_sealevel)
   
-  dr = interpolate.interp1d(dr_temp_x, dr_sealevel_array, kind='quadratic', fill_value='extrapolate')
-  
-  test_x = np.arange(-60,141)
-
-  st.line_chart(dr_sealevel_array)
-  st.line_chart(dr(test_x))
+  dr = interpolate.interp1d(dr_temp_x_input_tendegrees, dr_sealevel_array, kind='quadratic', fill_value='extrapolate')
   
   source = pd.DataFrame({
-  'x': test_x,
-  'f(x)': dr(test_x)
+  'Temp(F)': dr_temp_x_input_ondegrees,
+  'Density Ratio': dr(test_x)
   })
 
   c = alt.Chart(source).mark_line().encode(
-      x='x',
-      y='f(x)'
+      x='Temp(F)',
+      y='Density Ratio'
   )
   
   st.altair_chart(c, use_container_width = True)
