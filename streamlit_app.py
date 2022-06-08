c0472335b81e0e10e4e25a345e4c8c22766fbe5f
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-06-08 14:46:52 -0700

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -265,10 +265,12 @@ def main():
   
   runway_lengths = [4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000]
   runway_lengths_array = np.array(runway_lengths)
+  rwl_expanded = np.arange(4000, 12000, 100)
   
   if 34000 < user_ac_weight <= 38000:
     
     min_go_interpolated = interpolate.interp1d(runway_lengths_array, min_go_66_df.iloc[2,1:], kind='linear', fill_value='extrapolate')
+    mg_interp_array = min_go_interpolated(rwl_expanded)
     st.write(min_go_interpolated(6000))
   
   
